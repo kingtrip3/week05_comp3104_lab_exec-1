3bddb7bbb6eddfd9a64a1f94190c1efff8f9f0de
Matthew Mukherjee <matthew.mukherjee@georgebrown.ca>
2020-10-06 19:36:14 -0400

    updated

---
+++ build/precache-manifest.15daab1fa1a576a2d7c08637e1273479.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22e7d7971d403a8c9ce1",
+    "url": "/static/css/main.41eb07b3.chunk.css"
+  },
+  {
+    "revision": "22e7d7971d403a8c9ce1",
+    "url": "/static/js/main.22e7d797.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "752b5368f49b1a32e6d9205643b1fedb",
+    "url": "/index.html"
+  }
+];
